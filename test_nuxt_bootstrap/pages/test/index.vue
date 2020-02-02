0ce114ba15b5ddcea3480296f075014f2522909c
natsuki chubachi <natsukichubachi@gmail.com>
2020-02-02 22:59:51 +0900

    bootstrap-vue勉強用

---
+++ test_nuxt_bootstrap/pages/test/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        test_nuxt_bootstrap
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <!--
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+       -->
+      
+      <b-breadcrumb :items="items"></b-breadcrumb>
+      
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+      
+      <h2>Example heading <b-badge>New</b-badge></h2>
+      <h3>Example heading <b-badge>New</b-badge></h3>
+      <h4>Example heading <b-badge>New</b-badge></h4>
+      <h5>Example heading <b-badge>New</b-badge></h5>
+      <h6>Example heading <b-badge>New</b-badge></h6>
+      
+      <b-alert show>Default Alert</b-alert>
+      
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  data() {
+    return {
+      items: [
+        { text: "Admin", href: "#" },
+        { text: "Manage", href: "#" },
+        { text: "Library", active: true },
+      ]
+    }
+  },
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
